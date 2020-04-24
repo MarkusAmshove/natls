43914d4d5f9a3594a8a94f57e27420d6ad146960
Markus Amshove <scm@amshove.org>
2020-04-24 20:00:35 +0200

    Add tests for all single digit numbers

--- libs/natparse/src/test/java/org/amshove/natlint/natparse/lexing/LexingForNumbersShould.java
+++ libs/natparse/src/test/java/org/amshove/natlint/natparse/lexing/LexingForNumbersShould.java
@@ -49,4 +49,20 @@ public class LexingForNumbersShould extends AbstractLexerTest
 			dynamicTest("05121,001010",
 				() -> assertTokens("05121,001010", token(SyntaxKind.NUMBER, "05121,001010"))));
 	}
+
+	@TestFactory
+	Iterable<DynamicTest> lexAllSingleDigitNumbers()
+	{
+		return Arrays.asList(
+			dynamicTest("1", () -> assertTokens("1", token(SyntaxKind.NUMBER, "1"))),
+			dynamicTest("2", () -> assertTokens("2", token(SyntaxKind.NUMBER, "2"))),
+			dynamicTest("3", () -> assertTokens("3", token(SyntaxKind.NUMBER, "3"))),
+			dynamicTest("4", () -> assertTokens("4", token(SyntaxKind.NUMBER, "4"))),
+			dynamicTest("5", () -> assertTokens("5", token(SyntaxKind.NUMBER, "5"))),
+			dynamicTest("6", () -> assertTokens("6", token(SyntaxKind.NUMBER, "6"))),
+			dynamicTest("7", () -> assertTokens("7", token(SyntaxKind.NUMBER, "7"))),
+			dynamicTest("8", () -> assertTokens("8", token(SyntaxKind.NUMBER, "8"))),
+			dynamicTest("9", () -> assertTokens("9", token(SyntaxKind.NUMBER, "9"))),
+			dynamicTest("0", () -> assertTokens("0", token(SyntaxKind.NUMBER, "0"))));
+	}
 }
