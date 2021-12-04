edc632f3d3632fe7bf7bdf68ef62fe0301ed06af
Markus Amshove <scm@amshove.dev>
2021-12-04 14:17:16 +0100

    Print version information from Manifest

--- libs/natls/src/main/java/org/amshove/natls/App.java
+++ libs/natls/src/main/java/org/amshove/natls/App.java
@@ -5,8 +5,17 @@ import org.eclipse.lsp4j.launch.LSPLauncher;
 
 public class App
 {
+	private static final String APP_NAME = App.class.getPackage().getImplementationTitle();
+	private static final String APP_VERSION = App.class.getPackage().getImplementationVersion();
+
 	public static void main(String[] args)
 	{
+		if(args.length > 0)
+		{
+			System.out.printf("%s - Version %s%n", APP_NAME, APP_VERSION);
+			return;
+		}
+
 		var server = new NaturalLanguageServer();
 		var launcher = LSPLauncher.createServerLauncher(server, System.in, System.out);
 		server.connect(launcher.getRemoteProxy());
