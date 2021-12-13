810cb8754492480b3928011f2070bc9bec7352ee
Markus Amshove <scm@amshove.dev>
2021-12-13 21:05:47 +0100

    Rename Testclass

--- libs/natparse/src/test/java/org/amshove/natparse/lexing/LexerForMultiCharacterOperatorsShould.java
+++ libs/natparse/src/test/java/org/amshove/natparse/lexing/LexerForMultiCharacterOperatorsShould.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
-public class LexingForMultiCharacterOperatorsShould extends AbstractLexerTest
+public class LexerForMultiCharacterOperatorsShould extends AbstractLexerTest
 {
 	@Test
 	void lexColonEqualsAssign()
