3807d5e33d261fb3515f0413690ba29b218efd67
Markus Amshove <scm@amshove.dev>
2022-02-05 13:20:14 +0100

    Try to not return ITokenNode

--- libs/natparse/src/main/java/org/amshove/natparse/NodeUtil.java
+++ libs/natparse/src/main/java/org/amshove/natparse/NodeUtil.java
@@ -3,6 +3,7 @@ package org.amshove.natparse;
 import org.amshove.natparse.natural.INaturalModule;
 import org.amshove.natparse.natural.ISyntaxNode;
 import org.amshove.natparse.natural.ISyntaxTree;
+import org.amshove.natparse.natural.ITokenNode;
 
 public class NodeUtil
 {
@@ -20,6 +21,10 @@ public class NodeUtil
 		return findNodeAtPosition(line, character, module.syntaxTree());
 	}
 
+	/**
+	 * Tries to find the node at the given position.
+	 * It does try to not return an {@link ITokenNode}, but the node that contains the {@link ITokenNode}.
+	 */
 	public static ISyntaxNode findNodeAtPosition(int line, int character, ISyntaxTree syntaxTree)
 	{
 		ISyntaxNode previousNode = null;
@@ -33,7 +38,7 @@ public class NodeUtil
 
 			if (node.position().line() == line && node.position().offsetInLine() > character)
 			{
-				return previousNode;
+				return previousNode instanceof ITokenNode ? (ISyntaxNode) syntaxTree : previousNode;
 			}
 
 			if (node.position().line() > line)
