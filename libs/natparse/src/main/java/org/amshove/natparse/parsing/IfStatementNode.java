45abce55893d9b565d71f5a6eb96090077d7b1fe
Markus Amshove <scm@amshove.dev>
2022-06-15 22:47:43 +0200

    Add rudimentary parsing of if statements

---
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/IfStatementNode.java
@@ -0,0 +1,24 @@
+package org.amshove.natparse.parsing;
+
+import org.amshove.natparse.natural.IIfStatementNode;
+import org.amshove.natparse.natural.IStatementListNode;
+import org.amshove.natparse.natural.IStatementNode;
+
+public class IfStatementNode extends StatementListNode implements IIfStatementNode
+{
+	private StatementListNode body;
+
+	public IStatementListNode body()
+	{
+		return body;
+	}
+
+	void setBody(StatementListNode body)
+	{
+		for (var statement : body.statements())
+		{
+			addStatement((StatementNode) statement);
+		}
+		this.body = body;
+	}
+}
