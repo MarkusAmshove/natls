fd571a8476357ba4df450496a5b4db9c7b7aad92
Markus Amshove <scm@amshove.dev>
2022-08-18 19:59:03 +0200

    Make sure target directory for rules.xml exists

--- tools/ruletranslator/src/main/java/org/amshove/natqube/ruletranslator/App.java
+++ tools/ruletranslator/src/main/java/org/amshove/natqube/ruletranslator/App.java
@@ -11,6 +11,12 @@ public class App
 	public static void main(String[] args) throws IOException
 	{
 		System.out.println("Exporting rules to %s".formatted(args[0]));
+		var targetPath = Paths.get(args[0]);
+		var parentPath = targetPath.getParent();
+		if(!parentPath.toFile().exists())
+		{
+			parentPath.toFile().mkdirs();
+		}
 		var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<rules>\n";
 		xml += RuleRepository.getRules().stream().map(SonarRule::toXml).collect(Collectors.joining("\n"));
 		xml += "\n</rules>";
