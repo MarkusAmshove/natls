c882b68f659d60dddf44dbf62f92f2a675e30700
Markus Amshove <scm@amshove.dev>
2022-10-12 23:00:18 +0200

    Add private constructor

--- libs/natls/src/main/java/org/amshove/natls/CustomCommands.java
+++ libs/natls/src/main/java/org/amshove/natls/CustomCommands.java
@@ -4,4 +4,6 @@ public class CustomCommands
 {
 	public static final String CODELENS_NON_INTERACTIVE = "";
 	public static final String CODELENS_SHOW_REFERENCES = "natls.codelens.showReferences";
+
+	private CustomCommands() {}
 }
