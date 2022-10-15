bc43b85acae46a71b71d442dc966c9b06e0396e1
Markus Amshove <scm@amshove.dev>
2022-10-15 22:23:15 +0200

    Cleanup main

--- libs/natls/src/main/java/org/amshove/natls/App.java
+++ libs/natls/src/main/java/org/amshove/natls/App.java
@@ -3,12 +3,7 @@ package org.amshove.natls;
 import org.amshove.natls.languageserver.NaturalLanguageServer;
 import org.eclipse.lsp4j.launch.LSPLauncher;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -18,7 +13,7 @@ public class App
 	private static final String APP_VERSION = App.class.getPackage().getImplementationVersion();
 
 
-	public static void main(String[] args) throws IOException, URISyntaxException
+	public static void main(String[] args) throws IOException
 	{
 		if(args.length > 0)
 		{
@@ -26,22 +21,14 @@ public class App
 			return;
 		}
 
-		var logFile = Paths.get(System.getProperty("user.home"), ".natls.log");
-		if(!logFile.toFile().exists())
-		{
-			Files.writeString(logFile, "");
-		}
-		try(var logFileStream = new PrintStream(new FileOutputStream(logFile.toFile())))
-		{
-			LogManager.getLogManager().readConfiguration(App.class.getResourceAsStream("/logging.properties"));
+		LogManager.getLogManager().readConfiguration(App.class.getResourceAsStream("/logging.properties"));
 
-			var log = Logger.getAnonymousLogger();
-			log.info("Starting %s %s".formatted(APP_NAME, APP_VERSION));
+		var log = Logger.getAnonymousLogger();
+		log.info(() -> "Starting %s %s".formatted(APP_NAME, APP_VERSION));
 
-			var server = new NaturalLanguageServer();
-			var launcher = LSPLauncher.createServerLauncher(server, System.in, System.out);
-			server.connect(launcher.getRemoteProxy());
-			launcher.startListening();
-		}
+		var server = new NaturalLanguageServer();
+		var launcher = LSPLauncher.createServerLauncher(server, System.in, System.out);
+		server.connect(launcher.getRemoteProxy());
+		launcher.startListening();
 	}
 }
