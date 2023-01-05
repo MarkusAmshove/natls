f3d42bc113600320770062c06d1cd2204b803701
Claes Norreen <claes.norreen@tryg.dk>
2023-01-05 14:05:50 +0100

    Added support for GLOBAL USING GDA WITH BLOCK

---
+++ libs/natparse/src/main/java/org/amshove/natparse/natural/IBlockNode.java
@@ -0,0 +1,13 @@
+package org.amshove.natparse.natural;
+
+import org.amshove.natparse.lexing.SyntaxToken;
+
+public interface IBlockNode extends ISyntaxNode
+{
+	SyntaxToken parentblock();
+
+	SyntaxToken block();
+
+	IDefineData defineData();
+
+}
