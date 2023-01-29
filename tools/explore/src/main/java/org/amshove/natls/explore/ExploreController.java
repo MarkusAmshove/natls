907c6addedfa1554fdaa84fc6a2b629cf71d207a
Markus Amshove <scm@amshove.dev>
2023-01-29 20:48:44 +0100

    Render nodes

--- tools/explore/src/main/java/org/amshove/natls/explore/ExploreController.java
+++ tools/explore/src/main/java/org/amshove/natls/explore/ExploreController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -12,7 +13,12 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import org.amshove.natparse.lexing.Lexer;
+import org.amshove.natparse.lexing.SyntaxKind;
 import org.amshove.natparse.lexing.TokenList;
+import org.amshove.natparse.natural.ISyntaxTree;
+import org.amshove.natparse.natural.ITokenNode;
+import org.amshove.natparse.parsing.DefineDataParser;
+import org.amshove.natparse.parsing.StatementListParser;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
 
@@ -26,8 +32,9 @@ public class ExploreController
 	public Button parseButton;
 	public CodeArea tokenArea;
 	public CodeArea codeArea;
-	public TreeView nodeView;
+	public TreeView<NodeItem> nodeView;
 	public SplitPane codePane;
+	private Path selectedFilePath;
 
 	public void initialize()
 	{
@@ -41,6 +48,43 @@ public class ExploreController
 		var lexer = new Lexer();
 		var tokens = lexer.lex(codeArea.getText(), Path.of("MODULE.NSN"));
 		renderTokens(tokens);
+
+		renderNodes(tokens);
+	}
+
+	private void renderNodes(TokenList tokens)
+	{
+		nodeView.setRoot(null);
+		var root = new TreeItem<>(new NodeItem(null));
+		nodeView.setRoot(root);
+
+		tokens.rollback();
+		if(tokens.peek().kind() == SyntaxKind.DEFINE && tokens.peek(1).kind() == SyntaxKind.DATA)
+		{
+			var defineDataParser = new DefineDataParser(null);
+			var ddm = defineDataParser.parse(tokens).result();
+			addNodesRecursive(ddm, root);
+		}
+
+		var statementParser = new StatementListParser(null);
+		var statements = statementParser.parse(tokens);
+		for (var statement : statements.result().statements())
+		{
+			addNodesRecursive(statement, root);
+		}
+
+	}
+
+	private void addNodesRecursive(ISyntaxTree node, TreeItem<NodeItem> parent)
+	{
+		var root = new TreeItem<>(new NodeItem(node));
+
+		for (var descendant : node.descendants())
+		{
+			addNodesRecursive(descendant, root);
+		}
+
+		parent.getChildren().add(root);
 	}
 
 	private void renderTokens(TokenList tokens)
@@ -81,6 +125,7 @@ public class ExploreController
 			var source = Files.readString(file.toPath());
 			codeArea.clear();
 			codeArea.appendText(source);
+			selectedFilePath = file.toPath();
 		}
 		catch (IOException e)
 		{
@@ -91,5 +136,23 @@ public class ExploreController
 	public void codeAreaMouseClicked(MouseEvent mouseEvent)
 	{
 		tokenArea.scrollToPixel(codeArea.getEstimatedScrollX(), codeArea.getEstimatedScrollY());
+	}
+
+	private record NodeItem(ISyntaxTree node)
+	{
+		@Override
+		public String toString()
+		{
+			var tokenSource = "";
+			if(node != null)
+			{
+				tokenSource = node.directDescendantsOfType(ITokenNode.class).findFirst().map(tn -> tn.token().source()).orElse("?");
+			}
+			if(node instanceof ITokenNode tokenNode)
+			{
+				tokenSource = tokenNode.token().source();
+			}
+			return node != null ? "%s (%s)".formatted(node.getClass().getSimpleName(), tokenSource) : "root";
+		}
 	}
 }
