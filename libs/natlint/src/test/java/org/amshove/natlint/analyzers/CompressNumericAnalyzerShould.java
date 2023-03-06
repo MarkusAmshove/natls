888550a40ab3a5a637e58f8a878684c44e703031
Markus Amshove <scm@amshove.dev>
2023-03-06 23:17:44 +0100

    Add floating number type to compress numeric analyzer

--- libs/natlint/src/test/java/org/amshove/natlint/analyzers/CompressNumericAnalyzerShould.java
+++ libs/natlint/src/test/java/org/amshove/natlint/analyzers/CompressNumericAnalyzerShould.java
@@ -2,6 +2,8 @@ package org.amshove.natlint.analyzers;
 
 import org.amshove.natlint.linter.AbstractAnalyzerTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CompressNumericAnalyzerShould extends AbstractAnalyzerTest
 {
@@ -10,21 +12,24 @@ class CompressNumericAnalyzerShould extends AbstractAnalyzerTest
 		super(new CompressNumericAnalyzer());
 	}
 
-	@Test
-	void raiseADiagnosticIfAnOperandHasAFloatingNumberAndNoNumericIsPresent()
+	@ParameterizedTest
+	@ValueSource(strings = {
+		"N12,4", "F8"
+	})
+	void raiseADiagnosticIfAnOperandHasAFloatingNumberAndNoNumericIsPresent(String type)
 	{
 		testDiagnostics(
 			"""
 			DEFINE DATA
 			LOCAL
-			1 #VAR (N12,4)
+			1 #VAR (%s)
 			1 #TEXT (A) DYNAMIC
 			END-DEFINE
 
 			COMPRESS #VAR INTO #TEXT /* no NUMERIC present
 
 			END
-			""",
+			""".formatted(type),
 			expectDiagnostic(6, CompressNumericAnalyzer.COMPRESS_SHOULD_HAVE_NUMERIC)
 		);
 	}
