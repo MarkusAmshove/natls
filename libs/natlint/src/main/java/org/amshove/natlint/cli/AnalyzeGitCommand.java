2f986118b36b1a4aaa68b730438510964cb2e677
Markus Amshove <scm@amshove.dev>
2023-04-10 22:49:44 +0200

    Extract predicate parser

--- libs/natlint/src/main/java/org/amshove/natlint/cli/AnalyzeGitCommand.java
+++ libs/natlint/src/main/java/org/amshove/natlint/cli/AnalyzeGitCommand.java
@@ -1,6 +1,6 @@
 package org.amshove.natlint.cli;
 
-import org.amshove.natparse.natural.project.NaturalFile;
+import org.amshove.natlint.cli.git.GitStatusPredicateParser;
 import picocli.CommandLine;
 
 import java.io.BufferedReader;
@@ -9,7 +9,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.function.Predicate;
 
 @CommandLine.Command(name = "git-status", description = "Analyze files from `git status --porcelain`", mixinStandardHelpOptions = true)
 public class AnalyzeGitCommand implements Callable<Integer>
@@ -28,39 +27,18 @@ public class AnalyzeGitCommand implements Callable<Integer>
 				printUsage();
 				return 1;
 			}
-			gitChanges.add(line);
-		}
 
-		var interestingGitChanges = new ArrayList<String>();
-		for (var gitChange : gitChanges)
-		{
-			if (gitChange.startsWith("D"))
+			if (line.contains(".."))
 			{
-				continue;
-			}
-
-			var statusLine = gitChange.trim().split(" ", 2)[1];
-			if (gitChange.trim().startsWith("R"))
-			{
-				statusLine = statusLine.split("->", 2)[1].trim();
-			}
-
-			if (statusLine.startsWith(".."))
-			{
-				System.err.printf("Path seems to be relative: %s%nDid you forget to run git status with --porcelain ?%n", statusLine);
+				System.err.printf("Path seems to be relative: %s%nDid you forget to run git status with --porcelain ?%n", line);
 				printUsage();
 				return 1;
 			}
 
-			interestingGitChanges.add(statusLine);
+			gitChanges.add(line);
 		}
 
-		var filePredicates = new ArrayList<Predicate<NaturalFile>>();
-		for (String interestingGitChange : interestingGitChanges)
-		{
-			filePredicates.add(f -> f.getProjectRelativePath().equals(Paths.get(interestingGitChange)));
-		}
-
+		var filePredicates = new GitStatusPredicateParser().parseStatusToPredicates(gitChanges);
 		return new CliAnalyzer(
 			Paths.get(System.getProperty("user.dir")),
 			DiagnosticSinkType.STDOUT.createSink(),
