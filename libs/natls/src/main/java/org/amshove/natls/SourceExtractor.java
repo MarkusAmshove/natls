859160a56ec4d0260b2f71cefda4c03471be7225
Markus Amshove <scm@amshove.dev>
2023-05-03 22:07:13 +0200

    Test SourceExtractor

--- libs/natls/src/main/java/org/amshove/natls/SourceExtractor.java
+++ libs/natls/src/main/java/org/amshove/natls/SourceExtractor.java
@@ -1,6 +1,7 @@
 package org.amshove.natls;
 
 import org.amshove.natparse.natural.ISyntaxNode;
+import org.amshove.natparse.natural.ITokenNode;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -8,17 +9,31 @@ import java.nio.file.Files;
 
 public class SourceExtractor
 {
+	/**
+	 * Extracts the source of the node. If the node is not a {@link ITokenNode}, then the complete source of the
+	 * descendants will be taken, including whitespace.
+	 */
 	public static String extractSource(ISyntaxNode node)
 	{
 		try
 		{
-			var lines = Files.readAllLines(node.position().filePath());
-			for (var line = 0; line < lines.size(); line++)
+			if (node instanceof ITokenNode)
 			{
-				if (line == node.position().line())
+				var lines = Files.readAllLines(node.position().filePath());
+				for (var line = 0; line < lines.size(); line++)
 				{
-					return lines.get(line).substring(node.position().offsetInLine(), node.position().endOffset());
+					if (line == node.position().line())
+					{
+						return lines.get(line).substring(node.position().offsetInLine(), node.position().endOffset());
+					}
 				}
+			}
+			else
+			{
+				var source = Files.readString(node.position().filePath());
+				var startOffset = node.descendants().first().position().offset();
+				var endOffset = node.descendants().last().position().totalEndOffset();
+				return source.substring(startOffset, endOffset);
 			}
 
 			throw new IllegalStateException("Could not extract source");
