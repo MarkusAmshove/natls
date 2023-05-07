b9fa445bfd11b8f7bc87fd3a5c770b914b7c6f74
Markus Amshove <scm@amshove.dev>
2023-05-07 12:27:58 +0200

    Fix DECIDE ON branch descendants

--- libs/natparse/src/main/java/org/amshove/natparse/natural/IStatementVisitor.java
+++ libs/natparse/src/main/java/org/amshove/natparse/natural/IStatementVisitor.java
@@ -1,6 +1,5 @@
 package org.amshove.natparse.natural;
 
-
 @FunctionalInterface
 public interface IStatementVisitor
 {
--- libs/natparse/src/main/java/org/amshove/natparse/parsing/DecideOnBranchNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/DecideOnBranchNode.java
@@ -3,15 +3,13 @@ package org.amshove.natparse.parsing;
 import org.amshove.natparse.ReadOnlyList;
 import org.amshove.natparse.natural.IDecideOnBranchNode;
 import org.amshove.natparse.natural.IOperandNode;
-import org.amshove.natparse.natural.IStatementListNode;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class DecideOnBranchNode extends BaseSyntaxNode implements IDecideOnBranchNode
+class DecideOnBranchNode extends StatementWithBodyNode implements IDecideOnBranchNode
 {
 	private final List<IOperandNode> values = new ArrayList<>();
-	private IStatementListNode body;
 	private boolean hasValueRange;
 
 	@Override
@@ -26,20 +24,9 @@ class DecideOnBranchNode extends BaseSyntaxNode implements IDecideOnBranchNode
 		return hasValueRange;
 	}
 
-	@Override
-	public IStatementListNode body()
-	{
-		return body;
-	}
-
 	void addOperand(IOperandNode operand)
 	{
 		values.add(operand);
-	}
-
-	void setBody(IStatementListNode body)
-	{
-		this.body = body;
 	}
 
 	void setHasValueRange()
