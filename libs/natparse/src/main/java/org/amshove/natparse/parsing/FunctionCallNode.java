9e932eb9420edfd2a2cf9fc55c7b4524c7302379
Markus Amshove <scm@amshove.dev>
2023-06-15 11:42:24 +0200

    Remove unused import

--- libs/natparse/src/main/java/org/amshove/natparse/parsing/FunctionCallNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/FunctionCallNode.java
@@ -1,6 +1,5 @@
 package org.amshove.natparse.parsing;
 
-import org.amshove.natparse.natural.IDataType;
 import org.amshove.natparse.natural.IFunctionCallNode;
 
 class FunctionCallNode extends ModuleReferencingNode implements IFunctionCallNode
