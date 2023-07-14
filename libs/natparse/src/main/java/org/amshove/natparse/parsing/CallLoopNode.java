0e5b9b37538465f7145cf884f0ea6fe3098c0ba0
Claes65 <cnorreen@hotmail.com>
2023-07-14 16:09:59 +0200

    Parse CALL family and more

---
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/CallLoopNode.java
@@ -0,0 +1,43 @@
+package org.amshove.natparse.parsing;
+
+import org.amshove.natparse.ReadOnlyList;
+import org.amshove.natparse.natural.ICallLoopNode;
+import org.amshove.natparse.natural.IMutateVariables;
+import org.amshove.natparse.natural.IOperandNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CallLoopNode extends StatementWithBodyNode implements ICallLoopNode, IMutateVariables
+{
+	private IOperandNode calling;
+	private final List<IOperandNode> operands = new ArrayList<>();
+
+	@Override
+	public IOperandNode calling()
+	{
+		return this.calling;
+	}
+
+	@Override
+	public ReadOnlyList<IOperandNode> operands()
+	{
+		return ReadOnlyList.from(operands);
+	}
+
+	void setCalling(IOperandNode calling)
+	{
+		this.calling = calling;
+	}
+
+	void addOperand(IOperandNode operandNode)
+	{
+		operands.add(operandNode);
+	}
+
+	@Override
+	public ReadOnlyList<IOperandNode> mutations()
+	{
+		return ReadOnlyList.from(operands);
+	}
+}
