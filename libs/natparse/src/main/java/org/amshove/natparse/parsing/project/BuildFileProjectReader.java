66fe96f06d63acc65560ab766cb59f829fa62934
Markus Amshove <scm@amshove.dev>
2023-08-24 10:03:02 +0200

    Add folders from include/ directory as libraries (#354)
    closes #193

--- libs/natparse/src/main/java/org/amshove/natparse/parsing/project/BuildFileProjectReader.java
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/project/BuildFileProjectReader.java
@@ -7,7 +7,9 @@ import org.amshove.natparse.natural.project.NaturalProject;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BuildFileProjectReader
 {
@@ -27,14 +29,49 @@ public class BuildFileProjectReader
 	public NaturalProject getNaturalProject(Path buildfilePath)
 	{
 		var buildFileParser = new NaturalBuildFileParser(filesystem);
-		var naturalLibraries = buildFileParser.parseLibraries(buildfilePath);
+		var xmlLibraryDefinitions = buildFileParser.parseLibraries(buildfilePath);
 
 		var sourceDirectories = getSourceDirectories(buildfilePath);
-		removeSystemLibraries(naturalLibraries, sourceDirectories);
-		addSourcePaths(naturalLibraries, sourceDirectories);
+		var includeDirectories = getIncludeDirectories(buildfilePath);
 
-		var actualLibraries = mapXmlLibraries(naturalLibraries);
-		return new NaturalProject(buildfilePath.getParent(), actualLibraries);
+		removeLibrariesWithoutSourceFolders(xmlLibraryDefinitions, sourceDirectories, includeDirectories);
+		addSourcePaths(xmlLibraryDefinitions, sourceDirectories);
+
+		var actualLibraries = mapXmlLibraries(xmlLibraryDefinitions);
+		addLibrariesFromIncludeDirectory(actualLibraries, xmlLibraryDefinitions, includeDirectories);
+		return new NaturalProject(buildfilePath.getParent(), List.copyOf(actualLibraries.values()));
+	}
+
+	private void addLibrariesFromIncludeDirectory(Map<String, NaturalLibrary> naturalLibraries, List<XmlNaturalLibrary> xmlLibraries, List<Path> includePaths)
+	{
+		if (includePaths.isEmpty())
+		{
+			return;
+		}
+
+		var includeLibraries = includePaths.stream().map(NaturalLibrary::new).toList();
+		for (var includeLibrary : includeLibraries)
+		{
+			if (naturalLibraries.containsKey(includeLibrary.getName()))
+			{
+				throw new BuildFileParserException("Can't add library include/%s when a library with the same name exists under Natural-Libraries/".formatted(includeLibrary.getName()));
+			}
+
+			naturalLibraries.put(includeLibrary.getName(), includeLibrary);
+			// Go through the XML definitions again to see if the library from include/ is a steplib
+			// that we can now provide.
+			for (var xmlLibrary : xmlLibraries)
+			{
+				for (var steplib : xmlLibrary.getSteplibs())
+				{
+					if (steplib.equals(includeLibrary.getName()) && naturalLibraries.containsKey(steplib))
+					{
+						naturalLibraries.get(xmlLibrary.getName()).addStepLib(includeLibrary);
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	private void addSourcePaths(List<XmlNaturalLibrary> naturalLibraries, List<Path> sourceDirectories)
@@ -46,9 +83,11 @@ public class BuildFileProjectReader
 		}
 	}
 
-	private void removeSystemLibraries(List<XmlNaturalLibrary> naturalLibraries, List<Path> sourceDirectories)
+	private void removeLibrariesWithoutSourceFolders(List<XmlNaturalLibrary> naturalLibraries, List<Path> sourceDirectories, List<Path> includeDirectories)
 	{
-		var nonSystemLibraries = sourceDirectories.stream().map(p -> p.toFile().getName()).collect(Collectors.toList());
+		var nonSystemLibraries = Stream.concat(sourceDirectories.stream(), includeDirectories.stream())
+			.map(p -> p.toFile().getName())
+			.toList();
 
 		// Remove system-steplibs
 		for (var naturalLibrary : naturalLibraries)
@@ -63,9 +102,18 @@ public class BuildFileProjectReader
 		return filesystem.listDirectories(buildfilePath.getParent().resolve("Natural-Libraries"));
 	}
 
-	private List<NaturalLibrary> mapXmlLibraries(List<XmlNaturalLibrary> libraries)
+	private List<Path> getIncludeDirectories(Path buildFilePath)
 	{
-		var libraryMap = libraries.stream().collect(Collectors.toMap(XmlNaturalLibrary::getName, l -> new NaturalLibrary(l.getSourcePath())));
+		var includePath = buildFilePath.getParent().resolve("include");
+		return filesystem.exists(includePath)
+			? filesystem.listDirectories(includePath)
+			: List.of();
+	}
+
+	private Map<String, NaturalLibrary> mapXmlLibraries(List<XmlNaturalLibrary> libraries)
+	{
+		var libraryMap = libraries.stream().collect(Collectors.toMap(XmlNaturalLibrary::getName, this::mapXmlLibrary));
+		var systemLib = libraryMap.get("SYSTEM");
 
 		for (var library : libraries)
 		{
@@ -73,16 +121,25 @@ public class BuildFileProjectReader
 
 			for (var stepLib : library.getSteplibs())
 			{
-				theLibrary
-					.addStepLib(libraryMap.get(stepLib));
+				if (libraryMap.containsKey(stepLib))
+				{
+					theLibrary
+						.addStepLib(libraryMap.get(stepLib));
+				}
 			}
 
-			if (libraryMap.containsKey("SYSTEM"))
+			if (!library.getName().equals("SYSTEM") && systemLib != null)
 			{
-				theLibrary.addStepLib(libraryMap.get("SYSTEM"));
+				theLibrary.addStepLib(systemLib);
 			}
 		}
 
-		return List.copyOf(libraryMap.values());
+		return libraryMap;
+	}
+
+	private NaturalLibrary mapXmlLibrary(XmlNaturalLibrary lib)
+	{
+		var sourcePathUnderNaturalLibraries = lib.getSourcePath();
+		return new NaturalLibrary(sourcePathUnderNaturalLibraries);
 	}
 }
--- libs/natparse/src/test/java/org/amshove/natparse/parsing/project/BuildFileProjectReaderShould.java
+++ libs/natparse/src/test/java/org/amshove/natparse/parsing/project/BuildFileProjectReaderShould.java
@@ -1,6 +1,7 @@
 package org.amshove.natparse.parsing.project;
 
 import org.amshove.natparse.infrastructure.IFilesystem;
+import org.amshove.natparse.natural.project.NaturalLibrary;
 import org.amshove.natparse.natural.project.NaturalProject;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,8 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 class BuildFileProjectReaderShould
 {
 
-	private static final Path BUILD_FILE_PATH = Paths.get("some", "directory", "_naturalBuild");
+	private static final Path PROJECT_ROOT = Paths.get("some", "directory");
+	private static final Path BUILD_FILE_PATH = PROJECT_ROOT.resolve("_naturalBuild");
 
 	@Test
 	void returnANaturalProjectContainingTheBasePath()
@@ -45,7 +47,9 @@ class BuildFileProjectReaderShould
 
 		var naturalProject = createProject(fileSystem);
 		var naturalLibrary = naturalProject.getLibraries().get(0);
-		assertThat(naturalLibrary.getSourcePath()).isEqualTo(sourceDirectory("MYLIB"));
+
+		var sourceDirectory = Paths.get(BUILD_FILE_PATH.getParent().toString(), "Natural-Libraries", "MYLIB");
+		assertThat(naturalLibrary.getSourcePath()).isEqualTo(sourceDirectory);
 	}
 
 	@Test
@@ -75,7 +79,7 @@ class BuildFileProjectReaderShould
 	}
 
 	@Test
-	void linkStepLibsWithSystemLibrary()
+	void linkLibsWithSystemLibrary()
 	{
 		var fileSystem = new BuildFileBuilder(BUILD_FILE_PATH)
 			.addLibrary("SYSTEM")
@@ -84,19 +88,57 @@ class BuildFileProjectReaderShould
 			.toFileSystem();
 
 		var naturalProject = createProject(fileSystem);
-		var naturalLibrary = naturalProject.getLibraries().stream().filter(l -> l.getName().equals("MYLIB")).findFirst().orElseThrow(() -> new RuntimeException(""));
+		var naturalLibrary = assertHasLibrary(naturalProject, "MYLIB");
 		assertThat(naturalLibrary.getStepLibs().size()).isEqualTo(2);
 		assertThat(naturalLibrary.getStepLibs().get(1).getName()).isEqualTo("SYSTEM");
 	}
 
-	private Path sourceDirectory(String name)
+	@Test
+	void notLinkLibrariesWithThemself()
 	{
-		return Paths.get(BUILD_FILE_PATH.getParent().toString(), "Natural-Libraries", name);
+		var fileSystem = new BuildFileBuilder(BUILD_FILE_PATH)
+			.addLibrary("SYSTEM")
+			.addLibrary("MYLIB", "MYLIB")
+			.toFileSystem();
+
+		var project = createProject(fileSystem);
+		var myLib = assertHasLibrary(project, "MYLIB");
+		assertThat(myLib.getStepLibs()).as("Only one step lib to SYSTEM is expected").hasSize(1);
+		assertThat(myLib.getStepLibs().get(0).getName()).isEqualTo("SYSTEM");
+
+		var system = assertHasLibrary(project, "SYSTEM");
+		assertThat(system.getStepLibs()).as("SYSTEM shouldn't reference itself").isEmpty();
+	}
+
+	@Test
+	void createLibrariesThatArePresentInAnIncludeDirectoryNextToTheProjectFile()
+	{
+		// For example, SYSRPC is a library from SAG that is present on the server.
+		// It is not itself a library in the project file, but set as steplib for other libraries.
+		// We want those to be seen as library so that we can provide stub files in an include/ folder.
+
+		var fileSystem = new BuildFileBuilder(BUILD_FILE_PATH)
+			.addLibrary("MYLIB", "SYSRPC")
+			.addIncludeLibrary("SYSRPC")
+			.toFileSystem();
+
+		var project = createProject(fileSystem);
+		var myLib = assertHasLibrary(project, "MYLIB");
+		assertThat(myLib.getSourcePath()).isEqualTo(PROJECT_ROOT.resolve("Natural-Libraries").resolve("MYLIB"));
+
+		var sysRpc = assertHasLibrary(project, "SYSRPC");
+		assertThat(sysRpc.getSourcePath()).isEqualTo(PROJECT_ROOT.resolve("include").resolve("SYSRPC"));
+		assertThat(myLib.getStepLibs()).contains(sysRpc);
 	}
 
 	private NaturalProject createProject(IFilesystem fileSystem)
 	{
 		return new BuildFileProjectReader(fileSystem).getNaturalProject(BUILD_FILE_PATH);
+	}
+
+	private NaturalLibrary assertHasLibrary(NaturalProject project, String library)
+	{
+		return project.getLibraries().stream().filter(l -> l.getName().equals(library)).findFirst().orElseThrow();
 	}
 
 }
