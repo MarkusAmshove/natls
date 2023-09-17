73f898a08baf4b16903ce7fb6531df88ef06c939
Markus Amshove <scm@amshove.dev>
2023-09-17 10:56:26 +0200

    Add test for reparsing by define data change

--- libs/natls/src/test/java/org/amshove/natls/CallerReparseTest.java
+++ libs/natls/src/test/java/org/amshove/natls/CallerReparseTest.java
@@ -4,6 +4,7 @@ import org.amshove.natls.testlifecycle.LanguageServerTest;
 import org.amshove.natls.testlifecycle.LspProjectName;
 import org.amshove.natls.testlifecycle.LspTestContext;
 import org.amshove.natparse.lexing.LexerError;
+import org.amshove.natparse.parsing.ParserError;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -50,5 +51,45 @@ class CallerReparseTest extends LanguageServerTest
 		assertThat(subprogramFile.allDiagnostics())
 			.as("Diagnostic shouldn't be present anymore")
 			.noneMatch(d -> d.getCode().getLeft().equals(LexerError.INVALID_STRING_LENGTH.id()));
+	}
+
+	@Test
+	void changingDefineDataShouldTriggerReparsing(@LspProjectName("emptyproject") LspTestContext context)
+	{
+		testContext = context;
+
+		createOrSaveFile("LIBONE", "MYLDA.NSL", """
+			DEFINE DATA
+			LOCAL
+			END-DEFINE
+		""");
+
+		var subprog = createOrSaveFile("LIBONE", "MYSUB.NSN", """
+			DEFINE DATA
+			LOCAL USING MYLDA
+			END-DEFINE
+			WRITE #HI
+			END
+		""");
+
+		var subprogramFile = findLanguageServerFile(subprog);
+
+		// Diagnostic from copy code is propagated to the subprogram
+		assertThat(subprogramFile.allDiagnostics())
+			.as("Expected diagnostic not found")
+			.anyMatch(d -> d.getCode().getLeft().equals(ParserError.UNRESOLVED_REFERENCE.id()));
+
+		// Adding the variable to the LDA to fix the diagnostic
+		createOrSaveFile("LIBONE", "MYLDA.NSL", """
+			DEFINE DATA
+			LOCAL
+			1 #HI (A10)
+			END-DEFINE
+		""");
+
+		// Diagnostic from copy code should not be present on subprogram anymore, because it was reparsed
+		assertThat(subprogramFile.allDiagnostics())
+			.as("Diagnostic shouldn't be present anymore")
+			.noneMatch(d -> d.getCode().getLeft().equals(ParserError.UNRESOLVED_REFERENCE.id()));
 	}
 }
