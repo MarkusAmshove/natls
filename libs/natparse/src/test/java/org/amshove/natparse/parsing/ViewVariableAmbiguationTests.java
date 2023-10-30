05c8d6143a8579530708c0c56ff4f6080388c366
Markus Amshove <scm@amshove.dev>
2023-10-30 08:57:06 +0100

    Remove code smell

--- libs/natparse/src/test/java/org/amshove/natparse/parsing/ViewVariableAmbiguationTests.java
+++ libs/natparse/src/test/java/org/amshove/natparse/parsing/ViewVariableAmbiguationTests.java
@@ -3,7 +3,7 @@ package org.amshove.natparse.parsing;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
-public class ViewVariableAmbiguationTests extends ResourceFolderBasedTest
+class ViewVariableAmbiguationTests extends ResourceFolderBasedTest
 {
 	@TestFactory
 	Iterable<DynamicTest> testReferencing()
