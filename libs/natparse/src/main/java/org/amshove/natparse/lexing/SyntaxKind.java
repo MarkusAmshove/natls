2623633ff5dffa07c87561766e1758312da440b1
Markus Amshove <scm@amshove.dev>
2023-11-09 19:47:51 +0100

    Use go to definition to jump between opening and closing statement keywords (#418)
    With this change, `go to definition` can be used to jump between opening
    and closing tokens of a statement.
    
    For example, if it is triggered on `END-IF`, it will jump to the
    corresponding `IF` keyword.

--- libs/natparse/src/main/java/org/amshove/natparse/lexing/SyntaxKind.java
+++ libs/natparse/src/main/java/org/amshove/natparse/lexing/SyntaxKind.java
@@ -820,4 +820,14 @@ public enum SyntaxKind
 			this == UC ||
 			this == ZP;
 	}
+
+	public boolean closesStatement()
+	{
+		return this == END_IF || this == END_FOR || this == END_DECIDE || this == END_DEFINE || this == END_BREAK || this == END_ERROR || this == END_FIND || this == END_READ || this == END_SUBROUTINE || this == END_REPEAT || this == END_WORK;
+	}
+
+	public boolean opensStatement()
+	{
+		return this == IF || this == DEFINE || this == DECIDE || this == REPEAT || this == READ || this == FIND || this == FOR;
+	}
 }
