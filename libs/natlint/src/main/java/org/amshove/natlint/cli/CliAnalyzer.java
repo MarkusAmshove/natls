cb17116afb89740ac98dceea698775c9e07712e5
Markus Amshove <scm@amshove.dev>
2024-02-14 09:50:07 +0100

    Revert "Test fixed thread pool"
    This reverts commit 1f065b0d798fd6738bdda25066b00cc26676be26.

--- libs/natlint/src/main/java/org/amshove/natlint/cli/CliAnalyzer.java
+++ libs/natlint/src/main/java/org/amshove/natlint/cli/CliAnalyzer.java
@@ -22,7 +22,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Predicate;
@@ -109,10 +108,9 @@ public class CliAnalyzer
 		var indexEndTime = System.currentTimeMillis();
 
 		var startCheck = System.currentTimeMillis();
-		var executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 		for (var library : project.getLibraries())
 		{
-			library.files().parallelStream().forEach(file -> executor.submit(() ->
+			library.files().parallelStream().forEach(file ->
 			{
 				if (file.getFiletype() == NaturalFileType.DDM)
 				{
@@ -166,17 +164,13 @@ public class CliAnalyzer
 				totalDiagnostics.addAndGet(allDiagnosticsInFile.size());
 				diagnosticSink.printDiagnostics(filesChecked.get(), file.getPath(), allDiagnosticsInFile);
 				fileStatusSink.printStatus(file.getPath(), MessageType.SUCCESS);
-			}));
-
+			});
 			var currentMemory = Runtime.getRuntime().totalMemory();
 			if (currentMemory > maxMemoryInBytes)
 			{
 				maxMemoryInBytes = currentMemory;
 			}
-
 		}
-		executor.shutdown();
-		executor.close();
 
 		var endCheck = System.currentTimeMillis();
 
