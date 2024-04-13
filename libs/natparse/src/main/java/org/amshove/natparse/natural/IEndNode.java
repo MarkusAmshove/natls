b515f0c639bddfb5b1522a9d4654e8b9b5552647
Markus Amshove <scm@amshove.dev>
2024-04-13 10:12:43 +0200

    Parse `.` as END node

--- libs/natparse/src/main/java/org/amshove/natparse/natural/IEndNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/natural/IEndNode.java
@@ -1,4 +1,8 @@
 package org.amshove.natparse.natural;
 
+import org.amshove.natparse.lexing.SyntaxToken;
+
 public interface IEndNode extends IStatementNode
-{}
+{
+	SyntaxToken token();
+}
--- libs/natparse/src/main/java/org/amshove/natparse/parsing/EndNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/EndNode.java
@@ -1,6 +1,20 @@
 package org.amshove.natparse.parsing;
 
+import org.amshove.natparse.lexing.SyntaxToken;
 import org.amshove.natparse.natural.IEndNode;
 
 class EndNode extends StatementNode implements IEndNode
-{}
+{
+	private SyntaxToken endToken;
+
+	@Override
+	public SyntaxToken token()
+	{
+		return endToken;
+	}
+
+	void setEndToken(SyntaxToken endToken)
+	{
+		this.endToken = endToken;
+	}
+}
