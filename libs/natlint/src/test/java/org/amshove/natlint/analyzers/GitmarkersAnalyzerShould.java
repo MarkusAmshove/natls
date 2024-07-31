e8fec10c4673dc5c41e1bbab97ec6b73212204db
Claes Norreen <105717036+Claes65@users.noreply.github.com>
2024-07-31 12:24:50 +0200

    Add diagnostic for leftover git merge markers (#500)

---
+++ libs/natlint/src/test/java/org/amshove/natlint/analyzers/GitmarkersAnalyzerShould.java
@@ -0,0 +1,87 @@
+package org.amshove.natlint.analyzers;
+
+import org.amshove.natlint.linter.AbstractAnalyzerTest;
+import org.junit.jupiter.api.Test;
+
+class GitmarkersAnalyzerShould extends AbstractAnalyzerTest
+{
+	protected GitmarkersAnalyzerShould()
+	{
+		super(new GitmarkersAnalyzer());
+	}
+
+	@Test
+	void reportDiagnosticsForGitmarkersInLineComment()
+	{
+		configureEditorConfig("""
+			[*]
+			natls.style.discourage_gitmarkers=true
+			""");
+
+		testDiagnostics(
+			"""
+            DEFINE DATA LOCAL
+            1 A(A1)
+            END-DEFINE
+			* =======
+			""",
+			expectDiagnostic(3, GitmarkersAnalyzer.DISCOURAGED_GITMARKERS_IN_COMMENT)
+		);
+	}
+
+	@Test
+	void reportDiagnosticsForGitmarkersInInlineComment()
+	{
+		configureEditorConfig("""
+			[*]
+			natls.style.discourage_gitmarkers=true
+			""");
+
+		testDiagnostics(
+			"""
+            DEFINE DATA LOCAL /* <<<<<<< >>>>>>>
+            1 A(A1)
+            END-DEFINE
+			""",
+			expectDiagnostic(0, GitmarkersAnalyzer.DISCOURAGED_GITMARKERS_IN_COMMENT)
+		);
+	}
+
+	@Test
+	void reportNoDiagnosticsForNonGitmarkersInLineComment()
+	{
+		configureEditorConfig("""
+			[*]
+			natls.style.discourage_gitmarkers=true
+			""");
+
+		testDiagnostics(
+			"""
+            DEFINE DATA LOCAL
+            1 A(A1)
+            END-DEFINE
+			* << >>
+			""",
+			expectNoDiagnosticOfType(GitmarkersAnalyzer.DISCOURAGED_GITMARKERS_IN_COMMENT)
+		);
+	}
+
+	@Test
+	void reportNoDiagnosticsForNonGitmarkersInInlineComment()
+	{
+		configureEditorConfig("""
+			[*]
+			natls.style.discourage_gitmarkers=true
+			""");
+
+		testDiagnostics(
+			"""
+            DEFINE DATA LOCAL /* << >>
+            1 A(A1)
+            END-DEFINE
+			""",
+			expectNoDiagnosticOfType(GitmarkersAnalyzer.DISCOURAGED_GITMARKERS_IN_COMMENT)
+		);
+	}
+
+}
