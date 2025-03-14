d6973eae5ba779fb44114448fa9000fd0afe7ef7
Markus Amshove <scm@amshove.org>
2025-03-14 09:38:56 +0100

    Test File Type Measure

--- libs/natqube/src/test/java/org/amshove/natqube/NatlintSensorShould.java
+++ libs/natqube/src/test/java/org/amshove/natqube/NatlintSensorShould.java
@@ -1,43 +1,27 @@
 package org.amshove.natqube;
 
-import org.amshove.natqube.sensor.CsvDiagnostic;
-import org.amshove.natqube.sensor.NatlintSensor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
-import org.sonar.api.batch.sensor.internal.SensorContextTester;
-
 import java.nio.file.Path;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-class NatlintSensorShould
+import org.amshove.natqube.sensor.NatlintSensor;
+import org.junit.jupiter.api.Test;
+import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
+
+class NatlintSensorShould extends SonarQubeTest
 {
-	@TempDir
-	Path projectDirectory;
-
-	private final String projectKey = "projectKey";
-	private SensorContextTester context;
-
-	@BeforeEach
-	void setupContext()
-	{
-		context = SensorContextTester.create(projectDirectory);
-	}
-
 	@Test
 	void addAnIssue()
 	{
 		addNaturalFile("SUB.NSN", "DEFINE DATA LOCAL\n1 #A (A1)\nEND-DEFINE\n");
 
 		var diagnosticFile = new TestInputFileBuilder(projectKey, "natlint/diagnostics-1.csv")
-			.setProjectBaseDir(projectDirectory)
-			.setModuleBaseDir(projectDirectory)
+			.setProjectBaseDir(projectPath)
+			.setModuleBaseDir(projectPath)
 			.setContents(
 				"file;ruleId;severity;message;line;offset;length\n"
 					+ "%s;%s;%s;%s;%d;%d;%d\n".formatted(
-						projectDirectory.resolve(Path.of("Natural-Libraries/LIB/SRC/SUB.NSN")),
+						projectPath.resolve(Path.of("Natural-Libraries/LIB/SRC/SUB.NSN")),
 						"NL001",
 						"WARNING",
 						"Variable #A is unused",
@@ -54,17 +38,5 @@ class NatlintSensorShould
 		sensor.execute(context);
 
 		assertThat(context.allIssues()).hasSize(1);
-	}
-
-	void addNaturalFile(String name, String content)
-	{
-		var file = new TestInputFileBuilder(projectKey, "Natural-Libraries/LIB/SRC/%s".formatted(name))
-			.setProjectBaseDir(projectDirectory)
-			.setModuleBaseDir(projectDirectory)
-			.setLanguage(Natural.KEY)
-			.setContents(content)
-			.build();
-
-		context.fileSystem().add(file);
 	}
 }
---
+++ libs/natqube/src/test/java/org/amshove/natqube/measures/FileTypeMeasureShould.java
@@ -0,0 +1,110 @@
+package org.amshove.natqube.measures;
+
+import org.amshove.natqube.NaturalMetrics;
+import org.amshove.natqube.SonarQubeTest;
+import org.junit.jupiter.api.Test;
+
+public class FileTypeMeasureShould extends SonarQubeTest
+{
+	private final FileTypeMeasure sut = new FileTypeMeasure();
+
+	@Test
+	void countDdms()
+	{
+		var file = addNaturalFile("MYDDM.NSD", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_DDMS, 1, file);
+	}
+
+	@Test
+	void countSubprograms()
+	{
+		var file = addNaturalFile("MYDDM.NSN", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_SUBPROGRAMS, 1, file);
+	}
+
+	@Test
+	void countPrograms()
+	{
+		var file = addNaturalFile("MYDDM.NSP", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_PROGRAMS, 1, file);
+	}
+
+	@Test
+	void countExternalSubroutines()
+	{
+		var file = addNaturalFile("EXT.NSS", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_EXTERNAL_SUBROUTINES, 1, file);
+	}
+
+	@Test
+	void countHelpRoutines()
+	{
+		var file = addNaturalFile("HELP.NSH", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_HELPROUTINES, 1, file);
+	}
+
+	@Test
+	void countGdas()
+	{
+		var file = addNaturalFile("MYGDA.NSG", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_GDAS, 1, file);
+	}
+
+	@Test
+	void countLdas()
+	{
+		var file = addNaturalFile("LALDA.NSL", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_LDAS, 1, file);
+	}
+
+	@Test
+	void countPdas()
+	{
+		var file = addNaturalFile("UNEPDA.NSA", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_PDAS, 1, file);
+	}
+
+	@Test
+	void countMaps()
+	{
+		var file = addNaturalFile("MAPPI.NSM", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_MAPS, 1, file);
+	}
+
+	@Test
+	void countCopycodes()
+	{
+		var file = addNaturalFile("COPY.NSC", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_COPYCODES, 1, file);
+	}
+
+	@Test
+	void countFunctions()
+	{
+		var file = addNaturalFile("FUNC.NS7", "");
+
+		sut.measure(context, file);
+		assertMetric(NaturalMetrics.NUMBER_OF_FUNCTIONS, 1, file);
+	}
+
+}
