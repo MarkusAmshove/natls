95743b2aa9d25d4de754c7df562c18fbabf4692d
Markus Amshove <scm@amshove.org>
2025-04-08 15:29:01 +0200

    Remove caching of skipped number

--- libs/natparse/src/main/java/org/amshove/natparse/parsing/SkipOperandNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/SkipOperandNode.java
@@ -6,7 +6,6 @@ import org.amshove.natparse.natural.ISkipOperandNode;
 class SkipOperandNode extends BaseSyntaxNode implements ISkipOperandNode
 {
 	private SyntaxToken skipToken;
-	private int skipAmount = -1;
 
 	@Override
 	public SyntaxToken skipToken()
@@ -17,19 +16,8 @@ class SkipOperandNode extends BaseSyntaxNode implements ISkipOperandNode
 	@Override
 	public int skipAmount()
 	{
-		if (skipAmount >= 0)
-		{
-			return skipAmount;
-		}
-
 		var skipPart = skipToken.source().toLowerCase().replace("x", "");
-		if (!skipPart.matches("\\d+"))
-		{
-			return 0;
-		}
-
-		skipAmount = Integer.parseInt(skipPart);
-		return skipAmount;
+		return Integer.parseInt(skipPart);
 	}
 
 	void setSkipToken(SyntaxToken skipToken)
