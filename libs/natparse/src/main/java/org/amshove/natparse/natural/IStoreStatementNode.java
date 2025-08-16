f81df69f98a42cef64f12465c1048f7d2e0193bb
Markus Amshove <scm@amshove.org>
2025-08-16 11:35:52 +0200

    Make STORE not label referencable

--- libs/natparse/src/main/java/org/amshove/natparse/natural/IStoreStatementNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/natural/IStoreStatementNode.java
@@ -2,7 +2,7 @@ package org.amshove.natparse.natural;
 
 import org.jspecify.annotations.Nullable;
 
-public interface IStoreStatementNode extends IStatementNode, IAdabasAccessStatementNode, ILabelReferencable
+public interface IStoreStatementNode extends IStatementNode, IAdabasAccessStatementNode
 {
 	@Nullable
 	IOperandNode password();
--- libs/natparse/src/main/java/org/amshove/natparse/parsing/StoreStatementNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/parsing/StoreStatementNode.java
@@ -1,17 +1,15 @@
 package org.amshove.natparse.parsing;
 
-import org.amshove.natparse.lexing.SyntaxToken;
 import org.amshove.natparse.natural.IOperandNode;
 import org.amshove.natparse.natural.IStoreStatementNode;
 import org.amshove.natparse.natural.IVariableReferenceNode;
 import org.jspecify.annotations.Nullable;
 
-class StoreStatementNode extends StatementNode implements IStoreStatementNode, ILabelIdentifierSettable
+class StoreStatementNode extends StatementNode implements IStoreStatementNode
 {
 	private IVariableReferenceNode viewReference;
 	private IOperandNode password;
 	private IOperandNode cipher;
-	private SyntaxToken labelIdentifier;
 
 	@Override
 	public IVariableReferenceNode view()
@@ -44,17 +42,5 @@ class StoreStatementNode extends StatementNode implements IStoreStatementNode, I
 	void setCipher(IOperandNode cipher)
 	{
 		this.cipher = cipher;
-	}
-
-	@Override
-	public @Nullable SyntaxToken labelIdentifier()
-	{
-		return labelIdentifier;
-	}
-
-	@Override
-	public void setLabelIdentifier(SyntaxToken labelIdentifier)
-	{
-		this.labelIdentifier = labelIdentifier;
 	}
 }
